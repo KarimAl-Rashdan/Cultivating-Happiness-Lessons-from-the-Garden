baa7dc14e0566a435d2aeea6c31e01d39cf2c716
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-04-17 12:56:02 -0500

    add player sprite drawimage at supposed center of map

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const image = new Image()
 image.src = './assets/map.png'
 console.log(image)
 
+const playerImage = new Image()
+playerImage.src = './assets/playerDownHackathon.png'
+
 image.onload = () => {
-  c.drawImage(image, -750, -550)
+  c.drawImage(image, -785, -650)
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 8,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+   )
 }
